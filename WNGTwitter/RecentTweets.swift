ad1b68fb3ce92fc428b35fc080f2d7a6fdbca393
tomasz <tomasz@stechly.com>
2014-10-13 20:09:09 -0700

    Get tweets on refresh. Start app from table view. Avoid login screen which is not very interesting

--- WNGTwitter/RecentTweets.swift
+++ WNGTwitter/RecentTweets.swift
@@ -4,10 +4,19 @@
 //
 import UIKit
 import SwifteriOS
+import Accounts
+import Social
+
 
 class RecentTweets: UITableViewController {
-    
+
     var tweets : [JSONValue] = []
+
+    
+    @IBAction func refresh(sender: AnyObject) {
+        loginAndGetTweets()
+    }
+
     
     override func viewWillLayoutSubviews()
     {
@@ -22,6 +31,7 @@ class RecentTweets: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
         return tweets.count
     }
     
@@ -32,5 +42,71 @@ class RecentTweets: UITableViewController {
         
         return cell
     }
+    
+    
+    
+    
+    func loginAndGetTweets() {
+        let account = ACAccountStore()
+        let accountType = account.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        
+        account.requestAccessToAccountsWithType(accountType, options: nil,
+            completion: { (granted, error) in
+                if (granted) {
+                    
+                    let arrayOfAccount: NSArray = account.accountsWithAccountType(accountType)
+                    if (arrayOfAccount.count > 0) {
+                        let twitterAccount = arrayOfAccount.lastObject as ACAccount
+                        var swifter : Swifter
+                        swifter = Swifter(account: twitterAccount)
+                        
+                        self.fetchTwitterHomeStream(swifter)
+                    }
+                } else {
+                    self.alert("Unable to login")
+                }
+            }
+        )
+ 
+   }
+
+    
+
+    
+    
+    func fetchTwitterHomeStream(swifter: Swifter)
+    {
+        let failureHandler: ((NSError) -> Void) = {
+            error in
+            self.alert(error.localizedDescription)
+        }
+        
+        swifter.getStatusesHomeTimelineWithCount(20, sinceID: nil, maxID: nil, trimUser: true, contributorDetails: false, includeEntities: true, success: {
+            (statuses: [JSONValue]?) in
+            // Successfully fetched timeline, so lets create and push the table view
+            let tweetsViewController = self.storyboard!.instantiateViewControllerWithIdentifier("RecentTweets") as RecentTweets
+            
+            if statuses != nil {
+                tweetsViewController.tweets = statuses!
+                self.presentViewController(tweetsViewController, animated: true, completion: nil)
+            }
+            
+            }, failure: failureHandler)
+        
+    }
+    
+    
+    func alert(message: String)
+    {
+        // This is the iOS8 way of doing alerts. For older versions, look into UIAlertView
+        var alert = UIAlertController(
+            title: nil,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     
 }
